c1a8849cedf8a3718b0a5baa2d320a55f1f1a256
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-25 03:19:57 +0500

    given className

--- src/components/FormCard/HiddenCards.js
+++ src/components/FormCard/HiddenCards.js
@@ -4,15 +4,19 @@ import headerLogo from "./images/headerLogo.png";
 
 export const HiddenCards = () => {
     return (
-        <div>
-            <div>
+        <div className="hiddenCardContainer">
+            <div className="cardLogo">
                 <img src={headerLogo} height="35" />
             </div>
-            <div>
+            <div className="cardDetail">
                 <p>Uniswap pair</p>
                 <h2>UNCX / WETH</h2>
-                <a href="#"> <h3> 0xC70bB2736e218861DCa818d1e9f7A1930Fe61E5b <i class="fa fa-clone" aria-hidden="true"></i>
-                </h3> </a>
+                <a href="#"> 
+                <h3> 
+                0xC70bB2736e218861DCa818d1e9f7A1930Fe61E5b 
+                <i class="fa fa-clone" aria-hidden="true"></i>
+                </h3> 
+                </a>
                 <a href="#"> view on Uniswap </a>
             </div>
         </div>
