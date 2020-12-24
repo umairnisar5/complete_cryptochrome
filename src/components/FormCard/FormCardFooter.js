744df350955a74569a3363306eef1bcf1471e939
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-25 01:11:57 +0500

    Changing icon

--- src/components/FormCard/FormCardFooter.js
+++ src/components/FormCard/FormCardFooter.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./FormCardFooter.css";
 
- const FormCardFooter = () => {
+const FormCardFooter = () => {
     return (
         <div className="cardFormFooter">
             <div className="fa_logos">
-            <i className="fa fa-tree" aria-hidden="true"></i>
-            <i className="fa fa-tree" aria-hidden="true">100k</i>
-            <i className="fa fa-tree" aria-hidden="true">100k</i>
-            <i className="fa fa-tree" aria-hidden="true">100k</i>
+                <i className="fa fa-tree" aria-hidden="true"></i>
+                <i className="fa fa-tree" aria-hidden="true">100k</i>
+                <i className="fa fa-tree" aria-hidden="true">100k</i>
+                <i className="fa fa-tree" aria-hidden="true">100k</i>
             </div>
         </div>
     )
