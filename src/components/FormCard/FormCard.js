cd3051c4d3d22d51f68b893dfa46c543b7f1acfa
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-25 02:16:25 +0500

    Toggling tabs

--- src/components/FormCard/FormCard.js
+++ src/components/FormCard/FormCard.js
@@ -2,14 +2,26 @@ import React from 'react';
 import FormCardHeader from "./FormCardHeader";
 import FormCardDes from "./FormCardDes";
 import FormCardFooter from "./FormCardFooter";
+import FooterTabsView from "./FooterTabsView";
 import "./FormCard.css";
 
- const FormCard = () => {
+const FormCard = () => {
+    const [value, setValue] = React.useState(false);
+    const clickHandler = () => {
+        setValue(!value)
+        console.log(!value)
+    }
     return (
         <div className="formContainer">
-           <FormCardHeader /> 
-           <FormCardDes />
-           <FormCardFooter />
+            <div onClick={clickHandler}>
+                <FormCardHeader />
+                <FormCardDes />
+                <FormCardFooter />
+            </div>
+            {
+                value &&
+                <FooterTabsView />
+            }
         </div>
     )
 }
--- src/components/FormCard/FormCardFooter.js
+++ src/components/FormCard/FormCardFooter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./FormCardFooter.css";
-import FooterTabsView from "./FooterTabsView";
 
 const FormCardFooter = () => {
     return (
@@ -11,7 +10,6 @@ const FormCardFooter = () => {
                 <i className="fa fa-tree" aria-hidden="true">100k</i>
                 <i className="fa fa-tree" aria-hidden="true">100k</i>
             </div>
-            <FooterTabsView />
         </div>
     )
 }
