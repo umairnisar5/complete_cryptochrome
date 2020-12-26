249e5e04d110b63153fa2e26e65dd6bbe978df1f
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-26 20:56:12 +0500

    update cardcss

--- src/components/FormCard/HiddenCards2.js
+++ src/components/FormCard/HiddenCards2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import headerLogo from "./images/logo.png";
+import headerLogo from "./images/headerLogo.png";
 import {connect} from "react-redux";
 import "./HiddenCards.css";
 
@@ -7,7 +7,7 @@ export const HiddenCards2 = (props) => {
   return (
     <div className="hiddenCardContainer">
       <div className="cardLogo">
-        <img src={headerLogo} height="50" />
+        <img src={headerLogo} height="35" />
       </div>
       <div className="cardDetail">
         <p>Uniswap pair</p>
