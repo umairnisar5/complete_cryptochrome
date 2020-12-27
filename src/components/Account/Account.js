aee7dde0286e9ffb1e94c5dabf3277b42432b0c6
Alikhan219 <merabhai786786@gmail.com>
2020-12-27 13:41:13 -0800

    adding some new styles

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Index/Index.css";
 export default function Account() {
-    return (
-        <div className="">
-            <center>
-                <h1 style={{color:"#fff"}}></h1>
-                <div className="jumbotron" id="ticket">
-                    <img src="img/icon/farm_token.png" alt=""/>
-                        <p style={{color:"#fff", fontWeight:"bold"}}>Your not farming any tokens yet!</p>
-                        <Link to="/">
-                            <button className="btn btn-primary btn-lg" style={{background:"none",color:"#fff", border:"2px solid #fff"}}>View Farm</button>
-                        </Link>
-                </div>
-            </center>
+  return (
+    <div id="accountform-div">
+      <center>
+        <h1 style={{ color: "#fff" }}></h1>
+        <div className="jumbotron" id="ticket">
+          <img src="img/icon/farm_token.png" alt="" />
+          <p style={{ color: "#fff", fontWeight: "700" }}>
+            Your not farming any tokens yet!
+          </p>
+          <Link to="/">
+            <button
+              className="btn btn-primary btn-lg"
+              style={{
+                background: "none",
+                color: "#fff",
+                border: "2px solid #fff",
+                fontWeight: "700"
+              }}
+            >
+              View Farm
+            </button>
+          </Link>
         </div>
-  
-    )
+      </center>
+    </div>
+  );
 }
