4c12a5ac2d6de0916d3d21f242bd13d46291d8be
Alikhan219 <merabhai786786@gmail.com>
2020-12-27 08:11:05 -0800

    giving some styles to the header and fixing the header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = props => {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{marginLeft:"1100px"}}>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <div className="row">
                     <div className="col-sm-6">{top}</div>
                     <div className="col-sm-6">
