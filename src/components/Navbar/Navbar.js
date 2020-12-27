a9edd7abbd69d37bcf60018e56facd439a6eba46
Alikhan219 <merabhai786786@gmail.com>
2020-12-27 10:16:21 -0800

    fixing the connection button and logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,15 +10,16 @@ const Navbar = props => {
     const top = props.connected ? address : connectBtn;
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light" id="nav1">
+        
             <img src="img/logo.svg" width='50' alt=""/>
             <Link className="navbar-brand" to="/#" style={{color:"#fff", fontSize:"24px",fontWeight:"bold", marginLeft:"20px"}}> CryptoChrome</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <div className="row">
-                    <div className="col-sm-6">{top}</div>
-                    <div className="col-sm-6">
+                <div className="row" id="connect-div">
+                    <div className="con-btn">{top}</div>
+                    <div className="con-logo">
                         <button className="btn btn-warning"><span className="fa fa-cog"></span></button>
                     </div> 
                 </div>
